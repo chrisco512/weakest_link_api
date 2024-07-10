e4a34be4c4e7160ef025aae9d91248824e3db377
Chris Cordle <chrisco255@protonmail.com>
2024-07-10 11:50:47 +0200

    trivia api

---
+++ src/app/api/trivia/answer/route.ts
@@ -0,0 +1,11 @@
+import questions from "../../../data";
+
+export const dynamic = "force-dynamic";
+
+export function GET(request) {
+  const id = request.nextUrl.searchParams.get("id");
+
+  if (id) {
+    return new Response(JSON.stringify(questions[id].correct_answer));
+  }
+}
---
+++ src/app/data.js
@@ -0,0 +1,72 @@
+const quizQuestions = [
+  {
+    type: "multiple",
+    difficulty: "easy",
+    category: "Science: Mathematics",
+    question: "How many sides does a trapezium have?",
+    correct_answer: "4",
+  },
+  {
+    type: "multiple",
+    difficulty: "easy",
+    category: "Entertainment: Video Games",
+    question:
+      "In Rust, how many Timed Explosive Charges does it take to destroy a Ladder Hatch?",
+    correct_answer: "1",
+  },
+  {
+    type: "multiple",
+    difficulty: "hard",
+    category: "Science & Nature",
+    question: "Which of these chemical compounds is NOT found in gastric acid?",
+    correct_answer: "Sulfuric acid",
+  },
+  {
+    type: "multiple",
+    difficulty: "medium",
+    category: "Entertainment: Video Games",
+    question: "Which company developed Donkey Kong Country?",
+    correct_answer: "Rare",
+  },
+  {
+    type: "multiple",
+    difficulty: "medium",
+    category: "History",
+    question:
+      "What disease crippled President Franklin D. Roosevelt and led him to help the nation find a cure?",
+    correct_answer: "Polio",
+  },
+  {
+    type: "multiple",
+    difficulty: "medium",
+    category: "Entertainment: Film",
+    question:
+      'Which actor plays the character "Tommy Jarvis" in "Friday the 13th: The Final Chapter" (1984)?',
+    correct_answer: "Corey Feldman",
+  },
+  {
+    type: "multiple",
+    difficulty: "medium",
+    category: "Entertainment: Video Games",
+    question:
+      'In the game series "The Legend of Zelda", what was the first 3D game?',
+    correct_answer: "Ocarina of Time",
+  },
+  {
+    type: "boolean",
+    difficulty: "medium",
+    category: "Entertainment: Television",
+    question: "The television show Doctor Who first aired in 1963.",
+    correct_answer: "True",
+  },
+  {
+    type: "multiple",
+    difficulty: "medium",
+    category: "Entertainment: Music",
+    question:
+      'Madonna\'s song "Hung Up" includes a piece from which popular 70s song?',
+    correct_answer: "Gimmie! Gimmie! Gimme!",
+  },
+];
+
+export default quizQuestions;
